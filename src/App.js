de936b6cc973fc17b017b90165726ffd71f3052d
Tomas Vahalik <vahalto@brose.com>
2019-05-22 19:16:43 +0200

    First iteration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import avatar from "./img/avatar_op.png";
+import ln from "./img/linkedin-in-brands.svg";
+import gh from "./img/github-brands.svg";
+import email from "./img/envelope-regular.svg";
+import "./css/App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={avatar} className="App-logo" alt="logo" />{" "}
+        <h1>Tomáš Vahalík</h1>
+        <p>Full-Stack Webdeveloper</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <a href="https://www.linkedin.com/in/tomas-vahalik" target="_blank" rel="noopener noreferrer">
+            <img className="icon" src={ln} alt="LinkedIn" />
+          </a>
+
+          <a href="https://github.com/straiki" target="_blank" rel="noopener noreferrer">
+            <img className="icon" src={gh} alt="Git Hub" />
+          </a>
+
+          <a href="mailto:webcontact@vahalik.cz" target="_blank" rel="noopener noreferrer">
+            <img className="icon" src={email} alt="E-mail me!" />
+          </a>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <footer>
+        Made with <span aria-label="Love" role="img">❤️</span> using Font Awesome icons <a href="https://fontawesome.com/license" target="_blank" rel="noopener noreferrer" title="License"><span aria-label="Thanks" role="img">🙏</span></a>
+      </footer>
     </div>
   );
 }
